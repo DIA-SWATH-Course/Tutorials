37ec997bd9236a623193b4742e3c4a34a13e03c7
ibludau <bludau@imsb.biol.ethz.ch>
2018-06-25 14:04:00 +0200

    Update QE_run_library_generation_rstudio_win.sh
    export targets for skyline

--- QE_run_library_generation_rstudio_win.sh
+++ QE_run_library_generation_rstudio_win.sh
@@ -96,11 +96,11 @@ TargetedFileConverter \
 -out transitionlist_optimized_decoys.pqp \
 &>> Tutorial1_log.txt
 
-# convert to tsv for manual inspection
+# convert target library to tsv for manual inspection and Skyline usage
 # runtime ~1 min
 TargetedFileConverter \
--in transitionlist_optimized_decoys.TraML \
--out transitionlist_optimized_decoys.tsv \
-&>> Tutorial1_log.txt
+-in transitionlist_optimized.TraML \
+-out transitionlist_optimized.tsv \
+&>> Tutorial3_log.txt
 
 
